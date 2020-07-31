7e693904bc0f9ff217bbf3ee9c2562e175a222e3
memesmaker <40476349+memesmakers@users.noreply.github.com>
2020-07-31 10:07:07 -0400

    python installable package

---
+++ README.md
@@ -0,0 +1,5 @@
+package name - graphalchemy
+use python3
+install pip using "python3 -m pip install --user --upgrade pip"
+install virtualenv using "python3 -m pip install --user virtualenv"
+create a virtual environment using "python3 -m venv {environment name, say, venv}"
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='graphalchemy',
+    version='0.0.1',    
+    description='python graph package',
+    url='',
+    author='Smit Anand',
+    author_email='',
+    license='',
+    packages=['src'],
+    install_requires=[],
+)
