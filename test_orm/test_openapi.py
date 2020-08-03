aee0f88b7486be319fb64f7813008bc3f5ae7511
memesmaker <40476349+memesmakers@users.noreply.github.com>
2020-08-03 10:21:15 -0400

    added some more tests to public api

--- test_orm/test_openapi.py
+++ test_orm/test_openapi.py
@@ -1,11 +1,31 @@
 from src.graphalchemy.openapi import shortest_path, successor
 
 
-def test_shortest_path():
+def test_shortest_path_b_f_pass():
     path = shortest_path('b', 'f')
     assert path == ['b', 'c', 'f']
 
 
-def test_successor():
+def test_shortest_path_d_a_fail():
+    path = shortest_path('d', 'a')
+    assert path != ['b', 'c', 'f']
+
+
+def test_shortest_path_d_a_pass():
+    path = shortest_path('d', 'a')
+    assert path == ['d', 'b', 'a']
+
+
+def test_successor_b():
     next = successor('b')
     assert next == ['a', 'c']
+
+
+def test_successor_c():
+    next = successor('c')
+    assert next == ['f']
+
+
+def test_successor_d():
+    next = successor('d')
+    assert next == ['c', 'b']
